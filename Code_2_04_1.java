e6b2196a6186c4786b21f97db81521afc2f2571b
青木淳晟 <135562511+AokiJunsei@users.noreply.github.com>
2024-02-08 03:54:26 +0900

    Delete Code_2_04_1.java

--- Code_2_04_1.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-class Code_2_04_1 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		System.out.println(2 * N + 3);
-	}
-}
